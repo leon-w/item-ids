ee43d1e88c3253ff3fa4094de54d013a22fd4f64
Leon Wiemers <leonwiemers@gmail.com>
2021-05-19 13:13:26 +0200

    refactor

--- Readme.md
+++ Readme.md
@@ -1,17 +1,16 @@
-# item_ids
+# item-ids
 
 A program to fetch the item ids of steam items that are needed to use the buyorder API.
 
 ## Usage:
 ```
-usage: load.js [-h] appId {both,names,ids}
+usage: load.js [-h] appId
 
 positional arguments:
-  appId             App ID of the game or alias
-  {both,names,ids}  What data should be loaded
+  appId       App ID of the game or alias
 
 optional arguments:
-  -h, --help        show this help message and exit
+  -h, --help  show this help message and exit
 ```
 
 Results will get stored in the `data` folder.
--- load.js
+++ load.js
@@ -23,8 +23,10 @@ async function fetchHashNames(appId) {
                     norender: 1,
                 })
             ).body;
+
             allNames.push(...pageData.results.map(x => x.hash_name));
-            if (pageData.start + pageData.pagesize >= pageData.total_count) {
+            if (pageData.start + pageData.pagesize >= pageData.total_count || page == 2) {
+                console.log("Loaded all names.")
                 break;
             }
 
@@ -33,38 +35,28 @@ async function fetchHashNames(appId) {
                 console.log(`Loading names for app id ${appId}. ${pageData.total_count} items, ~${totalPages} pages`);
             }
 
-            console.log("Loaded page", page);
+            console.log(`Loaded page ${page}.`);
             page++;
+            
             await sleep(3000);
         } catch (err) {
-            console.log(`${err.toString()} | Failed to load page ${page}. Retrying after 3min...`);
+            console.log(`Failed to load page ${page}: ${err.toString()}. Retrying after 3min...`);
             await sleep(3 * 60 * 1000);
         }
     }
 
-    const dir = `data/${appId}`;
-    if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir);
-    }
-
     const namesFiltered = [...new Set(allNames)].sort();
-    fs.writeFileSync(`${dir}/names.json`, JSON.stringify(namesFiltered, null, 4));
-    console.log(`Saved ${namesFiltered.length} names to ${dir}/names.json`);
+
+    return namesFiltered;
 }
 
 async function fetchIds(appId) {
-    const dir = `data/${appId}`;
-    let hashNames;
-    try {
-        hashNames = JSON.parse(fs.readFileSync(`${dir}/names.json`, { encoding: "UTF-8" }));
-    } catch {
-        console.error(`Error: ${dir}/names.json not found. Make sure to load names first.`);
-        process.exit(1);
-    }
+    const file = `data/${appId}.json`;
+    const hashNames = await fetchHashNames(appId);
 
     let itemIds;
     try {
-        itemIds = JSON.parse(fs.readFileSync(`${dir}/ids.json`, { encoding: "UTF-8" }));
+        itemIds = JSON.parse(fs.readFileSync(file, { encoding: "UTF-8" }));
     } catch {
         itemIds = {};
     }
@@ -72,7 +64,7 @@ async function fetchIds(appId) {
     const missingItems = hashNames.filter(x => !(x in itemIds));
     const missingCount = missingItems.length;
 
-    console.log(`Fetching ${missingItems.length} ids.`);
+    console.log(`Fetching ${missingItems.length} new ids.`);
 
     for (let i = 0; i < missingCount; i++) {
         const hashName = missingItems[i];
@@ -86,32 +78,31 @@ async function fetchIds(appId) {
             console.log();
             const match = /Market_LoadOrderSpread\(\s*(\d+)\s*\)/.exec(html);
             if (match == null) {
-                console.error(`No id found for ${hashName}`);
+                console.error(`No id found for ${hashName}.`);
                 continue;
             }
-            const id = match[1];
+            const id = match[1].trim();
             itemIds[hashName] = id;
             try {
-                fs.writeFileSync(`${dir}/ids.json`, JSON.stringify(itemIds, Object.keys(itemIds).sort(), 4));
+                fs.writeFileSync(file, JSON.stringify(itemIds, Object.keys(itemIds).sort(), 4));
             } catch (err) {
-                console.error(`${err.toString()} | Failed to save progress.`);
+                console.error(`Failed to save progress: ${err.toString()}.`);
             }
             console.log(`(${i + 1}/${missingCount}) Loaded ${hashName}: ${id}`);
         } catch (err) {
-            console.log(`${err.toString()} | Failed to load id for ${hashName}`);
+            console.log(`Failed to load id for "${hashName}": ${err.toString()}.`);
             timeout = 5 * 60 * 1000;
             i--;
         }
         await sleep(timeout);
     }
-    console.log(`Saved ${missingItems.length} new ids to ${dir}/ids.json`);
+    console.log(`Saved ${missingItems.length} new ids to ${file}.`);
 }
 
 (async () => {
     const parser = new ArgumentParser();
 
     parser.add_argument("appId", { help: "App ID of the game or alias", type: String });
-    parser.add_argument("mode", { help: "What data should be loaded", choices: ["both", "names", "ids"] });
 
     const args = parser.parse_args();
 
@@ -124,16 +115,5 @@ async function fetchIds(appId) {
         }
     }
 
-    switch (args.mode) {
-        case "both":
-            await fetchHashNames(args.appId);
-            await fetchIds(args.appId);
-            break;
-        case "names":
-            await fetchHashNames(args.appId);
-            break;
-        case "ids":
-            await fetchIds(args.appId);
-            break;
-    }
+    await fetchIds(args.appId);
 })();
